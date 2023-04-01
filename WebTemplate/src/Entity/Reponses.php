4a39e3bbfd619ceed08b72fce3388d93d0bc51ca
MohamedHmaidi <mohamed.hmaidi@esprit.tn>
2023-04-01 17:32:33 +0100

    commit-1

---
+++ WebTemplate/src/Entity/Reponses.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Reponses
+ *
+ * @ORM\Table(name="reponses", indexes={@ORM\Index(name="idx_rec_id", columns={"rec_id"}), @ORM\Index(name="idx_user_id", columns={"user_id"})})
+ * @ORM\Entity
+ */
+class Reponses
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="rep_id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $repId;
+
+    /**
+     * @var int|null
+     *
+     * @ORM\Column(name="admin_id", type="integer", nullable=true)
+     */
+    private $adminId;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="rep_description", type="text", length=0, nullable=false)
+     */
+    private $repDescription;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date_rep", type="date", nullable=false, options={"default"="CURRENT_TIMESTAMP"})
+     */
+    private $dateRep = 'CURRENT_TIMESTAMP';
+
+    /**
+     * @var \User
+     *
+     * @ORM\ManyToOne(targetEntity="User")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="user_id", referencedColumnName="id_user")
+     * })
+     */
+    private $user;
+
+    /**
+     * @var \Reclamation
+     *
+     * @ORM\ManyToOne(targetEntity="Reclamation")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="rec_id", referencedColumnName="rec_id")
+     * })
+     */
+    private $rec;
+
+    public function getRepId(): ?int
+    {
+        return $this->repId;
+    }
+
+    public function getAdminId(): ?int
+    {
+        return $this->adminId;
+    }
+
+    public function setAdminId(?int $adminId): self
+    {
+        $this->adminId = $adminId;
+
+        return $this;
+    }
+
+    public function getRepDescription(): ?string
+    {
+        return $this->repDescription;
+    }
+
+    public function setRepDescription(string $repDescription): self
+    {
+        $this->repDescription = $repDescription;
+
+        return $this;
+    }
+
+    public function getDateRep(): ?\DateTimeInterface
+    {
+        return $this->dateRep;
+    }
+
+    public function setDateRep(\DateTimeInterface $dateRep): self
+    {
+        $this->dateRep = $dateRep;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function getRec(): ?Reclamation
+    {
+        return $this->rec;
+    }
+
+    public function setRec(?Reclamation $rec): self
+    {
+        $this->rec = $rec;
+
+        return $this;
+    }
+
+
+}
---
+++ WebTemplate/src/Entity/User.php
@@ -0,0 +1,188 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use App\Repository\UserRepository;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * User
+ *
+ * @ORM\Table(name="user")
+ *@ORM\Entity(repositoryClass = "App\Repository\UserRepository") */
+ 
+class User 
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id_user", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $idUser;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nom", type="string", length=255, nullable=false)
+     * @Assert\NotBlank(message="remplir le nom")
+     */
+    private $nom;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="prenom", type="string", length=255, nullable=false)
+     * @Assert\NotBlank(message="remplir le prenom")
+     */
+    private $prenom;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=255, nullable=false)
+     * @Assert\NotBlank(message="remplir votre Email")
+     * @Assert\Email(
+     *     message = "Email non valide")
+     */
+    private $email;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="mdp", type="string", length=255, nullable=true)
+     * @Assert\NotBlank(message="remplir votre mot de passe")
+   
+     */
+    private $mdp;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="tel", type="integer", nullable=false)
+     * @Assert\NotBlank(message="remplir votre téléphone")
+     * @Assert\Type(
+     *     type="integer",
+     *     message="il faut entrer une valeur numérique")
+     */
+    private $tel;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="image", type="text", length=0, nullable=false)
+  
+     */
+    private $image;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="role", type="string", length=50, nullable=false)
+      * @Assert\NotBlank(message="selectionner un role ")
+     */
+    private $role;
+
+    
+
+    
+
+    public function getIdUser(): ?int
+    {
+        return $this->idUser;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): self
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getMdp(): ?string
+    {
+        return $this->mdp;
+    }
+
+    public function setMdp(string $mdp): self
+    {
+        $this->mdp = $mdp;
+
+        return $this;
+    }
+
+    public function getTel(): ?int
+    {
+        return $this->tel;
+    }
+
+    public function setTel(int $tel): self
+    {
+        $this->tel = $tel;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getRole(): ?string
+    {
+        return $this->role;
+    }
+
+    public function setRole(string $role): self
+    {
+        $this->role = $role;
+
+        return $this;
+    }
+
+ 
+ 
+ 
+
+}
