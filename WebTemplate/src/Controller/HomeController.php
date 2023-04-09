b28557b33eaa301c53faf5754017ce41ce55ac57
boussada19 <boussada.achref@esprit.tn>
2023-04-09 23:43:15 +0200

    v6
    v6

--- WebTemplate/src/Controller/HomeController.php
+++ WebTemplate/src/Controller/HomeController.php
@@ -15,16 +15,4 @@ class HomeController extends AbstractController
             'controller_name' => 'HomeController',
         ]);
     }
-
-
-    #[Route('/admin', name: 'app_home_admin')]
-    public function indexAdmin(): Response
-    {
-        return $this->render('home/admin.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
-    }
-
-
-
 }
